0d0e82908a5ba64feaa4cbbc90314fb3cdd4d1ea
fauquembergue-edgar <154617408+fauquembergue-edgar@users.noreply.github.com>
2025-05-23 10:43:00 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 # config.py
+# Configuration de l application avec valeurs par defaut cadres
 import os
 from pydantic import BaseSettings, EmailStr, HttpUrl
 
 class Settings(BaseSettings):
     FLASK_ENV: str = "production"
-    SECRET_KEY: str
-    SMTP_HOST: str
-    SMTP_PORT: int
-    SMTP_USER: EmailStr
-    SMTP_PASS: str
-    BASE_URL: HttpUrl
+    SECRET_KEY: str = "your_secret_key"
+    SMTP_HOST: str = "smtp.gmail.com"
+    SMTP_PORT: int = 587
+    SMTP_USER: EmailStr = "fauquember@cy-tech.fr"
+    SMTP_PASS: str = "icvj rchk xsyt mgos"
+    BASE_URL: HttpUrl = "http://localhost:5000"
+
     MAX_PDF_SIZE_MB: int = 10
     SQLALCHEMY_DATABASE_URI: str = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:////tmp/signature.db")
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
