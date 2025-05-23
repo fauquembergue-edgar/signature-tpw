66eb30b96dea7aff207f6415bd1dd13800370dfb
fauquembergue-edgar <154617408+fauquembergue-edgar@users.noreply.github.com>
2025-05-23 10:03:59 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-# Configuration de l application
+# config.py
 import os
-from pydantic import BaseSettings, EmailStr, HttpUrl
+from pydantic_settings import BaseSettings
+from pydantic import EmailStr, HttpUrl
 
 class Settings(BaseSettings):
     FLASK_ENV: str = "production"
@@ -9,11 +10,9 @@ class Settings(BaseSettings):
     SMTP_PORT: int
     SMTP_USER: EmailStr
     SMTP_PASS: str
-    BASE_URL: HttpUrl  # URL de l application
+    BASE_URL: HttpUrl  # ex. https://monsite.com
+
     MAX_PDF_SIZE_MB: int = 10
-    # URI de la base de donnees: utilise Postgres en prod sur Render ou sqlite en local
-    SQLALCHEMY_DATABASE_URI: str = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:////tmp/signature.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
 
     class Config:
         env_file = ".env"
