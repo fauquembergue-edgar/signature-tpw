1eea1ec474c812ecd808c6c795a6ac5151be6a23
fauquembergue-edgar <154617408+fauquembergue-edgar@users.noreply.github.com>
2025-05-26 08:45:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -175,14 +175,11 @@ def status(session_id):
     done = all(f['signed'] for f in session_data['fields'])
     return f"<h2>Signature terminée : {'✅ OUI' if done else '❌ NON'}</h2>"
 
-def apply_text(pdf_path, x, y, text, scale=1.5):
+ef apply_text(pdf_path, x, y, text, scale=1.5):
     html_width, html_height = 852, 512
-    offset_y = 100
+    offset_x, offset_y = 15, 35
     pdf_width, pdf_height = letter
-
-    # Empêcher que y-offset soit négatif
-    y = max(offset_y, y)
-    x_pdf = x * (pdf_width / html_width)
+    x_pdf = (x + offset_x) * (pdf_width / html_width)
     y_pdf = pdf_height - ((y - offset_y) * (pdf_height / html_height))
 
     reader = PdfReader(pdf_path)
@@ -205,16 +202,12 @@ def apply_text(pdf_path, x, y, text, scale=1.5):
         writer.write(f)
 
 
-    with open(pdf_path, 'wb') as f:
-        writer.write(f)
-
-
 def apply_signature(pdf_path, sig_data, output_path, x, y, scale=1.5):
     width, height = 100, 40
     html_width, html_height = 852, 512
-    offset_y = 120
+    offset_x, offset_y = 1, 120
     pdf_width, pdf_height = letter
-    x_pdf = x * (pdf_width / html_width)
+    x_pdf = (x + offset_x) * (pdf_width / html_width)
     y_pdf = pdf_height - ((y - offset_y) * (pdf_height / html_height)) - (height / 2)
 
     if sig_data.startswith("data:image/png;base64,"):
@@ -243,7 +236,6 @@ def apply_signature(pdf_path, sig_data, output_path, x, y, scale=1.5):
 
     with open(output_path, 'wb') as f:
         writer.write(f)
-
 def save_signature_image(data_url, session_id, index):
     if data_url.startswith("data:image/png;base64,"):
         data_url = data_url.replace("data:image/png;base64,", "")
