907df8bf764b6b06b7d93285dc421636169f6f27
fauquembergue-edgar <154617408+fauquembergue-edgar@users.noreply.github.com>
2025-05-27 15:45:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ def status(session_id):
 
 def apply_text(pdf_path, x, y, text, scale=1.5):
     html_width, html_height = 852, 512
-    offset_x, offset_y = 40, 200
+    offset_x, offset_y = 40, 1
     pdf_width, pdf_height = letter
     x_pdf = (x) * (pdf_width / html_width)+ offset_x
     y_pdf = pdf_height - ((y) * (pdf_height / html_height)) - offset_y
@@ -204,7 +204,7 @@ def apply_text(pdf_path, x, y, text, scale=1.5):
 def apply_signature(pdf_path, sig_data, output_path, x, y, scale=1.5):
     width, height = 100, 40
     html_width, html_height = 852, 512
-    offset_x, offset_y = 70, 300
+    offset_x, offset_y = 70, 1
     pdf_width, pdf_height = letter
     x_pdf = (x) * (pdf_width / html_width) + offset_x
     y_pdf = pdf_height - ((y) * (pdf_height / html_height)) - (height / 2) - offset_y
@@ -244,7 +244,7 @@ def apply_checkbox(pdf_path, x, y, checked, scale=1.5):
     scale est conservé pour compatibilité, mais non utilisé.
     """
     html_width, html_height = 852, 512
-    offset_x, offset_y = 90, 200
+    offset_x, offset_y = 140, 1
     pdf_width, pdf_height = letter
 
     # Taille de la case
