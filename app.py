99d32587bae528f99cb91af600ee2ba6f68bc77b
fauquembergue-edgar <154617408+fauquembergue-edgar@users.noreply.github.com>
2025-05-28 09:53:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -376,6 +376,3 @@ def send_pdf_to_all(session_data):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
